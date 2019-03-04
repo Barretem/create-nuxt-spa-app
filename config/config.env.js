d3468de797e08343ba68cc8c4b68fb263fe8a2c1
barret <47966933+Barretem@users.noreply.github.com>
2019-03-04 21:05:00 +0800

    init project

---
+++ config/config.env.js
@@ -0,0 +1,19 @@
+/**
+ *  dev-env配置
+ *  Created by barret
+ */
+
+module.exports = {
+  dev: {
+    LINK_URL: '//zhidian.test.liefengtech.com',
+    DEBUG: true
+  },
+  test: {
+    LINK_URL: '//zhidian.test.liefengtech.com',
+    DEBUG: true
+  },
+  production: {
+    LINK_URL: '//zhidian.h5.liefengtech.com',
+    DEBUG: false
+  }
+}
